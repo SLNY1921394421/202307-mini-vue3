aa1ae683f85b5ab5a78fb50be1ef21d356ac3819
SLNY1921394421 <1921394421@qq.com>
2023-07-26 20:52:17 +0800

    实现 reactive 和 readonly 嵌套对象转换功能

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,9 +1,28 @@
-import { reactive } from '../reactive'
+import { isReactive, reactive } from '../reactive'
 describe("reactive", () => {
   it("happy path", () => {
     const original = { foo: 1 };
     const observed = reactive(original);
     expect(observed).not.toBe(original);
     expect(observed.foo).toBe(1);
-  })
+  });
+  test("nested reactives", () => {
+    const original = {
+      nested: {
+        foo: 1,
+      },
+      array: [{ bar: 2 }],
+    };
+    const observed = reactive(original);
+    expect(isReactive(observed.nested)).toBe(true);
+    expect(isReactive(observed.array)).toBe(true);
+    expect(isReactive(observed.array[0])).toBe(true);
+  });
+
+  // test("toRaw", () => {
+  //   const original = { foo: 1 };
+  //   const observed = reactive(original);
+  //   expect(toRaw(observed)).toBe(original);
+  //   expect(toRaw(original)).toBe(original);
+  // });
 })
